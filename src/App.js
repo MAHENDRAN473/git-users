9e3b5b527600a6fabe43255c72f67ff340ebf7a3
MAHENDRAN473 <mahendran.m@brandwatch.com>
2024-02-10 18:37:59 +0530

    cleanup & ocktakit import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Octokit } from "octokit";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const octokit = new Octokit({});
+  let [githubUserList, setGithubUserList] = useState([]);
+
+  useEffect(() => {
+    octokit
+      .request("GET /users", {
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      })
+      .then((result) => {
+        setGithubUserList(result.data);
+      });
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
